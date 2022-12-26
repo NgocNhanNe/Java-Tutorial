a6c0e237bcb39b2497adc89763deaf64b3348197
ngocnhanne <nguyenngocnhan2104@gmail.com>
2022-12-26 17:46:50 +0700

    clean code

--- src/Management_Library/Manage.java
+++ src/Management_Library/Manage.java
@@ -14,29 +14,28 @@ public class Manage {
             System.out.println("-------------------");
             System.out.print("Enter your choose: ");
             int choose = Integer.parseInt(scanner.nextLine());
-            switch (choose){
-                case 1:
+            switch (choose) {
+                case 1 -> {
                     System.out.println("Add new document");
                     System.out.println("-------------------");
                     addNewDocument();
-                    break;
-                case 2:
+                }
+                case 2 -> {
                     System.out.println("Delete document by id");
                     System.out.println("-------------------");
                     System.out.println("Enter id: ");
                     String id = scanner.nextLine();
-                    deleteDocumentbyID(id,documents);
+                    deleteDocumentByID(id, documents);
                     System.out.println("remove success");
-                    break;
-                case 3:
+                }
+                case 3 -> {
                     System.out.println("Show document by id");
                     System.out.println("-------------------");
                     System.out.println("Enter id: ");
                     String id1 = scanner.nextLine();
-                    showDocumentbyId(id1, documents);
-                    break;
-
-                case 4:
+                    showDocumentById(id1, documents);
+                }
+                case 4 -> {
                     System.out.println("Search document by type");
                     System.out.println("-------------------");
                     System.out.println("Type document: ");
@@ -45,30 +44,26 @@ public class Manage {
                     System.out.println("3.Paper");
                     System.out.println("Enter type: ");
                     int type = Integer.parseInt(scanner.nextLine());
-                    searchDocumentByType(type,documents);
-                    break;
-                case 5:
+                    searchDocumentByType(type);
+                }
+                case 5 -> {
                     System.out.println("List of documents: ");
                     showListDocuments(documents);
-                    break;
-                case 6:
-                    System.exit(1);
-                    break;
-                default:
+                }
+                case 6 -> System.exit(1);
+                default -> {
+                }
             }
         }
     }
 
-    public void searchDocumentByType(int type, List<Document> documents) {
-
+    public void searchDocumentByType(int type) {
         switch (type) {
             case 1 -> searchByBook();
             case 2 -> searchByMagazine();
             case 3 -> searchByPaper();
             default -> System.out.println("type is invalid!");
         }
-
-
     }
 
     public void searchByBook() {
@@ -86,7 +81,7 @@ public class Manage {
                 System.out.println(doc.toString()));
     }
 
-    private void showDocumentbyId(String id, List<Document> documents) {
+    private void showDocumentById(String id, List<Document> documents) {
         for (Document document: documents){
             if(document.getDoc_id().contains(id)){
                 System.out.println("Document is: "+document);
@@ -100,12 +95,8 @@ public class Manage {
         }
     }
 
-    private void deleteDocumentbyID(String id, List<Document> documents) {
-        for (Document document: documents){
-            if(document.getDoc_id().contains(id)){
-               documents.remove(document);
-            }
-        }
+    private void deleteDocumentByID(String id, List<Document> documents) {
+        documents.removeIf(document -> document.getDoc_id().contains(id));
         System.out.println(documents);
     }
 
@@ -126,39 +117,36 @@ public class Manage {
         System.out.println("3.Paper");
 
         int type = Integer.parseInt(scanner.nextLine());
-        switch (type){
-            case 1:
+        switch (type) {
+            case 1 -> {
                 System.out.println("Enter author name: ");
                 String author_name = scanner.nextLine();
-
                 System.out.println("Enter pages: ");
                 int pages = Integer.parseInt(scanner.nextLine());
-
-                Document book = new Book(doc_id,publisher,release_number,author_name,pages);
+                Document book = new Book(doc_id, publisher, release_number, author_name, pages);
                 documents.add(book);
                 System.out.println("Add new book successfully!");
                 System.out.println(book);
-                break;
-            case 2:
+            }
+            case 2 -> {
                 System.out.println("Enter issue number: ");
                 int issue_number = Integer.parseInt(scanner.nextLine());
                 System.out.println("Enter release_month: ");
                 int release_month = Integer.parseInt(scanner.nextLine());
-                Document magazine = new Magazine(doc_id,publisher,release_number,issue_number,release_month);
+                Document magazine = new Magazine(doc_id, publisher, release_number, issue_number, release_month);
                 documents.add(magazine);
                 System.out.println("Add new magazine successfully!");
                 System.out.println(magazine);
-                break;
-            case 3:
+            }
+            case 3 -> {
                 System.out.println("Enter release day: ");
                 int day = Integer.parseInt(scanner.nextLine());
-                Document paper = new Paper(doc_id,publisher,release_number,day);
+                Document paper = new Paper(doc_id, publisher, release_number, day);
                 documents.add(paper);
                 System.out.println("Add new paper successfully!");
                 System.out.println(paper);
-                break;
-            default:
-                System.out.println("type is invalid!");
+            }
+            default -> System.out.println("type is invalid!");
         }
 
     }
